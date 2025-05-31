3fb5b631636963e3ab2b4d70eaa49c572c402881
grand719 <lukasz.adam.pawlowski@gmail.com>
2025-05-31 23:27:42 +0200

    feat::Create curve creator

---
+++ src/CurveCreator.cpp
@@ -0,0 +1,79 @@
+#include "CurveCreator.h"
+
+namespace bezier
+{
+CurveCreator::CurveCreator() : mSelectedPoint{std::nullopt}, mIsDragging{false}
+{
+}
+
+void CurveCreator::OnRightPress(sf::Vector2f &position)
+{
+    CreatPoint(position);
+    CreateCubicBezier();
+}
+
+void CurveCreator::CreatPoint(sf::Vector2f &pointPostion)
+{
+    mPointsList.push_back(std::make_shared<Point>(pointPostion));
+}
+
+void CurveCreator::CreateCubicBezier()
+{
+    if (mPointsList.size() == 2)
+    {
+        sf::Vector2f point0 = mPointsList[0].get()->getPosition();
+        sf::Vector2f point1 = mPointsList[1].get()->getPosition();
+
+        mCubicBezierList.push_back(std::make_shared<CubicBezier>(point0, point0, point1, point1));
+        mPointsList.clear();
+    }
+}
+
+void CurveCreator::Render(sf::RenderWindow &window)
+{
+    for (const auto point : mPointsList)
+    {
+        point.get()->Render(window);
+    }
+
+    for (const auto curve : mCubicBezierList)
+    {
+        curve.get()->Render(window);
+    }
+}
+
+void CurveCreator::OnMouseMove(sf::Vector2f &position)
+{
+    if (mIsDragging && mSelectedPoint != std::nullopt)
+    {
+
+        mSelectedPoint.value()->setPosition(position);
+    }
+}
+
+void CurveCreator::OnLeftPress(sf::Vector2f &mousePos)
+{
+    const float hitRadius = 10.0f;
+    for (const auto curve : mCubicBezierList)
+    {
+        auto controlPoints = curve.get()->getAllControlPoints();
+        for (auto &pointRef : controlPoints)
+        {
+            bezier::Point &point = pointRef.get();
+            sf::Vector2f pointPos = point.getPosition();
+            float distance = std::sqrt(std::pow(mousePos.x - pointPos.x, 2) + std::pow(mousePos.y - pointPos.y, 2));
+            if (distance <= hitRadius)
+            {
+                mIsDragging = true;
+                mSelectedPoint = &point;
+                break;
+            }
+        }
+    }
+}
+void CurveCreator::OnMouseLeftRelease()
+{
+    mSelectedPoint = std::nullopt;
+    mIsDragging = false;
+}
+} // namespace bezier
---
+++ src/CurveCreator.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "CubicBezier.h"
+#include "Point.h"
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+namespace bezier
+{
+class CurveCreator
+{
+  public:
+    CurveCreator();
+    void OnRightPress(sf::Vector2f &position);
+    void Render(sf::RenderWindow &window);
+
+    void OnMouseMove(sf::Vector2f &position);
+    void OnLeftPress(sf::Vector2f &position);
+    void OnMouseLeftRelease();
+
+  private:
+    void CreatPoint(sf::Vector2f &pointPostion);
+    void CreateCubicBezier();
+
+    std::vector<std::shared_ptr<CubicBezier>> mCubicBezierList;
+    std::vector<std::shared_ptr<Point>> mPointsList;
+    std::optional<bezier::Point *> mSelectedPoint;
+    bool mIsDragging;
+};
+} // namespace bezier
